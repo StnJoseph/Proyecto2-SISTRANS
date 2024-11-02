63b6858305dbd6da37da43a616efcb994c15d890
Samuel Charry <s.charryt@uniandes.edu.co>
2024-11-02 11:47:39 -0500

    ofrecen contructor y DB datos

--- src/main/java/uniandes/edu/co/proyecto/modelo/OfrecenPK.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/OfrecenPK.java
@@ -12,7 +12,32 @@ public class OfrecenPK {
     private Proveedor proveedor_nit;
 
     @ManyToOne
-    @JoinColumn(name = "producto_codigodebarras",referencedColumnName =   "codigo_de_barras") 
+    @JoinColumn(name = "producto_codigodebarras", referencedColumnName = "codigo_de_barras") 
     private Producto producto_codigodebarras;
 
+    public OfrecenPK() {
+        super();
+    }
+
+    public OfrecenPK(Proveedor proveedor_nit, Producto producto_codigodebarras) {
+        super();
+        this.proveedor_nit = proveedor_nit;
+        this.producto_codigodebarras = producto_codigodebarras;
+    }
+
+    public Proveedor getProveedor_nit() {
+        return proveedor_nit;
+    }
+
+    public void setProveedor_nit(Proveedor proveedor_nit) {
+        this.proveedor_nit = proveedor_nit;
+    }
+
+    public Producto getProducto_codigodebarras() {
+        return producto_codigodebarras;
+    }
+
+    public void setProducto_codigodebarras(Producto producto_codigodebarras) {
+        this.producto_codigodebarras = producto_codigodebarras;
+    }
 }
